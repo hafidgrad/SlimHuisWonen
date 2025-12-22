7236580b48d6b23d4587ffbc5109eff9f6ef2599
hafidgrad <hafidgrad@gmail.com>
2025-12-22 22:55:27 +0100

    Fix slug for Philips Hue product

--- data/products.js
+++ data/products.js
@@ -1,6 +1,6 @@
 export const products = [
   {
-    slug:affiliateUrl: "https://www.amazon.nl/dp/B0991VHWSJ?tag=slimhuiswonen-21",
+    slug: "philips-hue-white-ambiance-starterkit",
     brand: "Philips Hue",
     title: "Philips Hue White Ambiance Starterkit",
     shortTitle: "Hue White Ambiance kit",
