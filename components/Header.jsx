c735ee44f3faba60a3b76f41a9cf0af77a2eb61b
hafidgrad <hafidgrad@gmail.com>
2025-12-27 08:30:56 +0100

    Improve Header with Next Image and correct navigation

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,22 +1,28 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className="site-header">
       <div className="container nav-container">
-        <div className="logo-wrapper">
-          <Link href="/" className="logo-link">
-            <img src="/logo.png" alt="SlimHuisWonen logo" className="logo" />
-            <span className="site-title">SlimHuisWonen.nl</span>
-          </Link>
-        </div>
+        <Link href="/" className="logo-link">
+          <Image
+            src="/logo.png"
+            alt="SlimHuisWonen logo"
+            width={40}
+            height={40}
+            className="logo"
+            priority
+          />
+          <span className="site-title">SlimHuisWonen.nl</span>
+        </Link>
 
         <nav className="main-nav">
-          <a href="#over">Over</a>
-          <a href="#categorieen">Categorieën</a>
-          <a href="#aanraders">Aanraders</a>
-          <a href="#blog">Tips &amp; uitleg</a>
-          <a href="#contact">Contact</a>
+          <Link href="/over">Over</Link>
+          <Link href="/categorieen">Categorieën</Link>
+          <Link href="/aanraders">Aanraders</Link>
+          <Link href="/tips">Tips & uitleg</Link>
+          <Link href="/contact">Contact</Link>
           <Link href="/producten" className="nav-highlight">
             Producten
           </Link>
