01c9e5c02179b7d71a727b6e98458d9d17c2950e
hafidgrad <hafidgrad@gmail.com>
2026-01-20 11:30:04 +0100

    Fix TipBanner alignment on tips pages

--- components/TipBanner.jsx
+++ components/TipBanner.jsx
@@ -3,27 +3,22 @@ import Image from "next/image";
 export default function TipBanner({ src, alt }) {
   return (
     <div className="tipBanner">
-      {/* Blurred background */}
-      <div className="tipBannerBlur">
-        <Image
-          src={src}
-          alt={alt}
-          fill
-          priority
-          sizes="(max-width: 768px) 100vw, 1100px"
-          style={{ objectFit: "cover" }}
-        />
-      </div>
+      <div
+        className="tipBannerBlur tipBannerBlurLeft"
+        style={{ backgroundImage: `url(${src})` }}
+      />
+      <div
+        className="tipBannerBlur tipBannerBlurRight"
+        style={{ backgroundImage: `url(${src})` }}
+      />
 
-      {/* Sharp centered image */}
       <div className="tipBannerInner">
         <Image
           src={src}
           alt={alt}
-          fill
+          width={1024}
+          height={1024}
           priority
-          sizes="(max-width: 768px) 100vw, 900px"
-          style={{ objectFit: "contain" }}
         />
       </div>
     </div>
